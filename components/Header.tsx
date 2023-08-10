b0e22d1cdf7013d729095b435840750383d2221e
eholster <ehols001@odu.edu>
2023-08-10 10:56:20 -0400

    styling update

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,16 +3,16 @@ import { userInfo } from '@/constants'
 
 const Header = () => {
     return (
-        <div className="flex justify-between items-center h-1/5 bg-gradient-to-r from-indigo-900 p-2 lg:p-4 m-4 lg:m-8 mb-2 lg:mb-4 rounded-t-lg">
-            <div className='flex w-2/3 h-full overflow-hidden'>
+        <div className="flex items-center h-1/5 bg-gradient-to-r from-indigo-900 m-4 lg:m-8 mb-2 lg:mb-4 rounded-t-lg">
+            <div className='flex w-2/3 h-full p-2 lg:p-4'>
                 <Image
                     src={userInfo.picture}
                     alt="Profile Picture"
-                    className='h-full w-auto rounded-full'
+                    className='w-auto h-full rounded-full'
                 />
-                <div className='flex flex-col justify-evenly ml-2 lg:ml-4 overflow-hidden'>
-                    <p className='text-lg md:text-xl lg:text-5xl font-bold tracking-wide'>{userInfo.name}</p>
-                    <p className='text-xs md:text-sm lg:text-xl truncate'>
+                <div className='flex flex-col w-auto h-full justify-evenly ml-2 lg:ml-4 overflow-hidden'>
+                    <p className='text-lg/6 md:text-xl/6 lg:text-5xl font-bold tracking-wide'>{userInfo.name}</p>
+                    <p className='text-xs/5 md:text-sm/5 lg:text-xl truncate'>
                         {userInfo.titles.map((title) => (
                             title.title
                         ))}
