47a68ae1f02d31152871a1d97abdbd8fb78e8df5
eholster <ehols001@odu.edu>
2023-08-10 10:56:06 -0400

    sizing update

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import Navbar from '@/components/Navbar'
 
 export default function Home() {
     return (
-        <main className='w-full h-screen p-8 md:p-12'>
-            <div className='flex flex-col w-full h-full bg-slate-800 rounded-xl border-2 border-slate-500'>
+        <main className='w-full h-screen p-8'>
+            <div className='flex flex-col w-full h-full min-h-[300px] bg-slate-800 rounded-xl border-2 border-slate-500'>
                 <Header />
                 <div className='flex flex-row h-4/5 overflow-hidden'>
                     <Navbar />
