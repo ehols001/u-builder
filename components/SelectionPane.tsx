0fcc917b9dbceb95d50d99e93ae45f203b1019a2
eholster <ehols001@odu.edu>
2023-09-14 18:48:05 -0400

    removed

--- components/SelectionPane.tsx
+++
@@ -1,9 +0,0 @@
-const SelectionPane = () => {
-    return (
-        <div className="basis-5/6 bg-slate-500 p-2 lg:p-4 m-4 lg:m-8 mt-2 lg:mt-4 ml-2 lg:ml-4 rounded-lg">
-            SelectionPane
-        </div>
-    )
-}
-
-export default SelectionPane;
