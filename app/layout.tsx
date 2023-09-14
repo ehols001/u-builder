c203617364f95e9f04c6da429a9590bd2dad2cd3
eholster <ehols001@odu.edu>
2023-09-14 18:48:52 -0400

    setup actual app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,30 @@
-import './globals.css'
+import Footer from '@/components/Footer'
+import Navbar from '@/components/Navbar'
+import '@/styles/globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'YouBuilder',
-  description: 'Generate Your Own Web Portfolio',
+    title: 'YouBuilder',
+    description: 'Generate Your Own Web Portfolio',
 }
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <Navbar />
+                <div className='bg-gradient-radial flex justify-center items-center'>
+                    {children}
+                </div>
+                <Footer />
+            </body>
+        </html>
+    )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,9 @@
-import SelectionPane from '@/components/SelectionPane'
-import Header from '@/components/Header'
-import Navbar from '@/components/Navbar'
+import Display from '@/components/Display'
 
 export default function Home() {
     return (
-        <main className='w-full h-screen p-8'>
-            <div className='flex flex-col w-full h-full min-h-[300px] bg-slate-800 rounded-xl border-2 border-slate-500'>
-                <Header />
-                <div className='flex flex-row h-4/5 overflow-hidden'>
-                    <Navbar />
-                    <SelectionPane />
-                </div>
-            </div>
-        </main>
+        <div className=''>
+            <Display />
+        </div>
     )
 }
