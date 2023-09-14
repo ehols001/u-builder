56374f87677ff795ef17fb5f131025f4511dbe33
eholster <ehols001@odu.edu>
2023-09-14 18:52:12 -0400

    developed dynamic navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,32 @@
+import '@/styles/globals.css'
 import Link from "next/link";
-import { navbarLinks } from "@/constants";
+import Image from 'next/image';
+import { navbar_left, navbar_right, user_details } from "@/lib/app-details";
+import { NavbarTab } from "./NavbarTab";
 
 const Navbar = () => {
     return (
-        <div className="basis-1/6 bg-gradient-to-b from-indigo-900 to-indigo-950 p-2 lg:p-4 m-4 lg:m-8 mt-2 lg:mt-4 mr-2 lg:mr-4 rounded-lg text-xs md:text-base lg:text-lg overflow-hidden hover:overflow-y-auto">
-            <div className="flex flex-col divide-y">
-                {navbarLinks.map((link, index) => (
-                    <Link href={`/${link.id}`} key={index} className="py-2 truncate">
-                        {link.title}
-                    </Link>
+        <div className="flex justify-evenly w-3/4 md:w-2/3 lg:w-1/2 mx-auto my-4 bg-gradient-to-b from-transparent via-slate-300/40 to-transparent rounded-b-3xl ">
+            <div className="navbar">
+                {navbar_left.map((link, index) => (
+                    <NavbarTab key={index} link={link} from='left' />
+                ))}
+            </div>
+            <div className="flex justify-center border-2 border-b-amber-300/20 border-x-transparent border-t-transparent -mx-0.5">
+                <Link href={'/'} className="-my-2">
+                    <Image
+                        src={user_details.picture}
+                        width={100}
+                        height={100}
+                        alt="Profile Picture"
+                        sizes="(min-width: 100px)"
+                        className='min-w-full border-4 border-amber-300/50 rounded-full hover:bg-slate-100/30'
+                    />
+                </Link>
+            </div>
+            <div className="navbar">
+                {navbar_right.map((link, index) => (
+                    <NavbarTab key={index} link={link} from='right' />
                 ))}
             </div>
         </div>
